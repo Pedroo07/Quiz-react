2dabaee6f2d0952771468b1c7b7e3b2a4f1866de
Pedroo07 <123650104+Pedroo07@users.noreply.github.com>
2023-10-04 22:48:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# quiz-react
+ Primeiro projeto utilizando react
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Welcome from './components/welcome'
+import './App.css'
+import { useContext, useEffect } from 'react'
+import Question from './components/Question'
+import { QuizContext } from './context/quiz'
+import GameOver from './components/GameOver'
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" })
+  }, [])
+
+  return (
+    <div className='App'>
+      <h1>Quiz de Progamação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
+  )
+}
+
+export default App
